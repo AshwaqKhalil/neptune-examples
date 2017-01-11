34b238d48d34e0beca300ad6b30489ec3cd5a9b8
Łukasz Ptaszyński <lukasz.ptaszynski@codilime.com>
2017-01-11 10:06:22 +0100

    NPT-1324 Add missing licencses to examples
    Change-Id: I8ee5bb6ecb0cdc3016deee50f720e7088f086a27

--- python/flower-species-prediction/main.py
+++ python/flower-species-prediction/main.py
@@ -1,3 +1,28 @@
+#
+# All contributions by The TensorFlow Authors:
+# Copyright (c) 2016, The TensorFlow Authors.
+# All rights reserved
+#
+# All contributions by deepsense.io:
+# Copyright (c) 2017, deepsense.io
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# The original source code can be found at:
+# https://www.tensorflow.org/versions/r0.11/tutorials/tflearn/
+#
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
