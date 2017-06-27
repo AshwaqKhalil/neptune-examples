68ec8c5a5e5056f647698bbd305d8d120a6cf86b
Piotr Migdał <pmigdal@gmail.com>
2017-06-27 18:14:12 +0200

    the easiest integration via tensorboard

--- 2-not-mnist-dashboard/main.py
+++ 2-not-mnist-dashboard/main.py
@@ -1,49 +1,44 @@
-import pandas as pd
 from scipy import io
 import numpy as np
 from keras.utils import np_utils
 from sklearn.model_selection import train_test_split
 
-# pip install git+git://github.com/stared/keras-sequential-ascii.git
-from keras_sequential_ascii import sequential_model_to_ascii_printout
-
-from keras.callbacks import Callback
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten
 from keras.layers import Conv2D, MaxPool2D, Dropout
+from keras.callbacks import TensorBoard
 
+from deepsense import neptune
 
-# XXX data loading in a different file?
+# the easiest integration
+ctx = neptune.Context()
+ctx.integrate_with_tensorflow()
+tbCallBack = TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
+
 # wget http://yaroslavvb.com/upload/notMNIST/notMNIST_small.mat
 # load data
 data = io.loadmat("notMNIST_small.mat")
-
-# transform data
 X = data['images']
 y = data['labels']
 
-# 28x28 images, grayscale
-resolution = 28
-# 10 letters: ABCDEFGHIJ
-classes = 10
+resolution = 28  # 28x28 images, grayscale
+classes = 10     # 10 letters: ABCDEFGHIJ
 
+# transforming data for TensorFlow backend
 X = np.transpose(X, (2, 0, 1))
-
-y = y.astype('int32')
+X = X.reshape((-1, resolution, resolution, 1))
 X = X.astype('float32') / 255.
 
-# channel for X
-X = X.reshape((-1, resolution, resolution, 1))
-
 # 3 -> [0., 0., 0., 1., 0., 0., 0., 0., 0., 0.]
-Y = np_utils.to_categorical(y, 10)
+y = y.astype('int32')
+Y = np_utils.to_categorical(y, classes)
 
 # splitting data into training and test sets
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y,
                                                     test_size=0.20,
                                                     random_state=137)
 
-# create network
+# create neural network architeture
 
 model = Sequential()
 
@@ -62,10 +57,10 @@ model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-# visualize network architecture
-sequential_model_to_ascii_printout(model)
 
 model.fit(X_train, Y_train,
           epochs=10,
           batch_size=32,
-          validation_data=(X_test, Y_test), callbacks=[])
+          validation_data=(X_test, Y_test),
+          verbose=2,
+          callbacks=[tbCallBack])
