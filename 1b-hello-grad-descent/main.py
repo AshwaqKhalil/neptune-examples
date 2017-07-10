a0d07e76c767be7772ede3b7b00a0528d3aee011
Piotr Migdał <pmigdal@gmail.com>
2017-07-10 12:19:54 +0200

    matching update for 1b

--- 1b-hello-grad-descent/main.py
+++ 1b-hello-grad-descent/main.py
@@ -4,27 +4,27 @@ import neptuner
 
 # function y = f(x)
 def f(x):
-    return (x - 6)**2 + 9
+    return x**2 - 14 * x + 7
 
 # symbolic gradient
 def df(x):
-    return 2 * (x - 6)
+    return 2 * x - 14
 
 # parameters
-x = 0.         # initial x
-lr = 0.1       # learning rate
-n_steps = 30   # number of iterations
+learning_rate = 0.1
+n_steps = 30
+x = 0.
 
 channel_x = neptuner.numeric_channel('x')
 channel_y = neptuner.numeric_channel('y')
 
 # searching for minimum with gradient descent
 for step in range(n_steps):
-    x -= lr * df(x)
+    x -= learning_rate * df(x)
 
     # logging new x and y
     channel_x.send(step, x)
     channel_y.send(step, f(x))
 
-    # a crucial part of SleepyGradientDescent
-    sleep(1)
+    # and a crucial part of the SleepyGradientDescent algorithm ;)
+    sleep(1.)
